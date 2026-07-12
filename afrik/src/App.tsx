76c6cd36687abfb19dea44330a13b025c4e00ed2
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-12 15:36:18 +0300

    will be back

---
+++ afrik/src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return <>
+  
+   <div>
+      <h1>Mind boggling</h1>
+      <p>Niggas in the trenches hukua na a lot of unique knowledge about random stuff that will leave you mind blown, unique but useless. For example ukitaka kuhara unakula cabbage, avocado na chapo then watch the magic unfold. Now that's unique knowledge but at the same time it's useless</p>
+    </div>
+  
+  </>
+   
+  
+}
+
+
+export default App
